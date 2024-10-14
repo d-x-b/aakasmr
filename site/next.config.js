8bef974a869de4e55d6ffec0f4a532a23c98bc39
d-x-b <15963850+d-x-b@users.noreply.github.com>
2024-10-13 21:20:08 -0700

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,13 +4,13 @@ const nextConfig = {
   env: {
     name: 'A Kasm Registry',
     description: 'Some Kasm workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://github.com/d-x-b/aakasmregistry/',
-    contactUrl: 'https://github.com/d-x-b/aakasmregistry/support',
+    icon: 'https://user-images.githubusercontent.com/5698566/230345149-ef757e51-6eb9-479d-94f5-a13e4ad33b03.png',
+    listUrl: 'https://d-x-b.github.io/aakasmr/',
+    contactUrl: 'https://d-x-b.github.io/aakasmr/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/tree/1.0',
+  basePath: '/aakasmr/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
